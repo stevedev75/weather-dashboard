5bdf0d2d1ef59567e5c7171f3395a2cfedde0273
Steve Dever <stephendever.cr@gmail.com>
2021-04-07 11:49:14 -0400

    add updated JS per askBCS advice

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,26 @@ var currentTemp = document.getElementById("curTemp");
 var currentHum = document.getElementById("curHum");
 var currentWind = document.getElementById("curWind");
 var currentUv = document.getElementById("curUv");
-var subButton = document.getElementsByClassName("btn");
+var subMit = document.getElementById("sbutton");
+var form = document.getElementById("user-form");
+
+
+console.log(cityName.value);
+
 var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=b6ac08ddf42292bbdd1f3dae08ee40e4";
 
-subButton.addEventListener('click', function (e) {
+//subMit.addEventListener('click', function (e) {
+
+form.addEventListener('submit', function (e) {    
     e.preventDefault();
-    fetch(url).then(function (response) {
+     fetch(url).then(function (response) {
             if (response.ok) {
                 return response.json();
+                
     }})
         })
+
+
+      
+        console.log(form);
+        //console.log(url);
